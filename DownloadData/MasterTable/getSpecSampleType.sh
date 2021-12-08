92025197a594d7284b34d9269b52fb25ab27c2ed
Pierre Luisi <pierrespc@ml21-1040.local>
2021-12-08 18:51:12 +0100

    improver script to get specific sample type info in masterTable

--- DownloadData/MasterTable/getSpecSampleType.sh
+++ DownloadData/MasterTable/getSpecSampleType.sh
@@ -3,6 +3,6 @@
 table=$1
 st=$2
 
-awk -v s="$st" -F "\t" 'BEGIN{num=0; } {if(NR==1){for(i=1;i<=NF;i=i+1){if(match($i,s)){num=num+1;col[num]=i}}};for(i=1;i<=num;i=i+1){colN=col[i]; printf "%s\t", $colN}; print ""}' $table
+awk -v s="$st" -F "\t" 'BEGIN{num=0; } {if(NR==1){for(i=1;i<=NF;i=i+1){if(match($i,s)){num=num+1;col[num]=i}}};for(i=1;i<=num;i=i+1){colN=col[i]; printf "%s\t", $colN}; print ""}' $table | awk -F "\t" -v s="$st" -v OFS="\t" '{if($1!="NA")print $0}' | uniq 
 
 
